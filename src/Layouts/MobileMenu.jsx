33b3ca5818b383de4831d068f40ebb9aada60943
wahsandaruwan <wahsandaruwan6@gmail.com>
2022-09-21 15:36:52 +0530

    Updated li margin and cnetered mobile menu links

--- src/Layouts/MobileMenu.jsx
+++ src/Layouts/MobileMenu.jsx
@@ -2,12 +2,18 @@ const MobileMenu = () => {
   return (
     <>
       <ul className="absolute top-[105px] left-0 w-full bg-blue-200 px-8">
-        <li className="border-b-2 border-zinc-200 w-full">Home</li>
-        <li className="border-b-2 border-zinc-200 w-full">About</li>
-        <li className="border-b-2 border-zinc-200 w-full">Hosting</li>
-        <li className="border-b-2 border-zinc-200 w-full">Domain</li>
-        <li className="border-b-2 border-zinc-200 w-full">SSL</li>
-        <li className="border-b-2 border-zinc-200 mb-3 w-full">Contact</li>
+        <li className="border-b-2 border-zinc-200 w-full text-center">Home</li>
+        <li className="border-b-2 border-zinc-200 w-full text-center">About</li>
+        <li className="border-b-2 border-zinc-200 w-full text-center">
+          Hosting
+        </li>
+        <li className="border-b-2 border-zinc-200 w-full text-center">
+          Domain
+        </li>
+        <li className="border-b-2 border-zinc-200 w-full text-center">SSL</li>
+        <li className="border-b-2 border-zinc-200 mb-3 w-full text-center">
+          Contact
+        </li>
         <div className="flex justify-center my-5">
           <button className="mr-3">Login</button>
           <button>Register</button>
